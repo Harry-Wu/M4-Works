ce9e6710dda590c52cb3e70731eb158dd649bcd9
Harry <wuxiaohuaer@gmail.com>
2016-09-10 22:24:33 +0800

    ex7-5 v1:显示单张图片

---
+++ ex7-5 - lcd pic/user/inc/key.h
@@ -0,0 +1,32 @@
+#ifndef _KEY_H_
+#define _KEY_H_
+//#include "stm32f407xx.h"
+#include "stm32f4xx.h" 
+#include "sys.h"
+#include "MyTypeDef.h"
+//#define KEY1   ((GPIOA->IDR>>0) &0X01)
+//#define KEY2   ((GPIOE->IDR>>2) &0X01)
+//#define KEY3   ((GPIOE->IDR>>3) &0X01)
+//#define KEY4   ((GPIOE->IDR>>4) &0X01)
+ 
+#define KEY1 PAin(0)
+#define KEY2 PEin(2)
+#define KEY3 PEin(3)
+#define KEY4 PEin(4)
+ 
+#define NO_KEY   0XFF
+#define KEY1_OK     1
+#define KEY2_OK 	2
+#define KEY3_OK 	3
+#define KEY4_OK	   4 
+
+
+
+
+
+void key_init(void);
+u8  key_scanf(void);
+
+#endif
+
+
---
+++ ex7-5 - lcd pic/user/src/delay.c
@@ -0,0 +1,25 @@
+#include "stm32f4xx.h" 
+#include "delay.h"
+#include "MyTypeDef.h"
+
+//延时n微秒
+//参数n: us
+void delay_us(uint32_t n)
+{
+	SysTick->LOAD =n*SYSCLK/8; //设定计数次数
+	SysTick->VAL =0;     //重装载（重装载寄存器的值复制到计数器）
+	SysTick->CTRL |=0x01; //开systick定时器
+	while(!(SysTick->CTRL&(0x01<<16)));
+	SysTick->CTRL &=~(0x01); //关systick定时器
+}
+
+//延时n毫秒
+//参数n: ms  (最大延时798.9ms)
+void delay_ms(u32 n)
+{
+	SysTick->LOAD =n*SYSCLK*1000/8; //设定计数次数
+	SysTick->VAL =0;     //重装载（重装载寄存器的值复制到计数器）
+	SysTick->CTRL |=0x01; //开systick定时器
+	while(!(SysTick->CTRL&(0x01<<16)));
+	SysTick->CTRL &=~(0x01); //关systick定时器
+}
