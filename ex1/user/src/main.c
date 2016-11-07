22f11c392415a399525d12bee144dc775e0a030b
Harry <wuxiaohuaer@gmail.com>
2016-11-07 18:17:50 +0800

    V1.1: 增加位带操作, 项目格式升级为uvprojx

--- ex1/user/src/main.c
+++ ex1/user/src/main.c
@@ -1,25 +1,43 @@
+/*****************************************************************************
+@File name:  
+@Description: 闪烁LED 
+@Author: Harry Wu
+@Version: V1.1
+@Date: 2016-11-7
+@History: 
+		V1.0: 最基本的操作寄存器
+		V1.1: 增加位带操作, 项目格式升级为uvprojx
+
+		
+*****************************************************************************/
 #include "stm32f4xx.h"
+//#include "key.h"
+//#include "delay.h"
+#include "io_bit.h"
+#define D5  PFout(9)
+#define D6  PFout(10)
+#define LS1 PFout(8)
 
 void led_init(void)
 {
 	//端口时钟使能PF口
 	RCC->AHB1ENR |= 1<<5;
-	//设置管脚--PF9,PF10设置为普通功能、推挽、无上下拉功输出
+	//设置管脚PF9----D5设置为普通功能、推挽、无上下拉功输出
 	GPIOF->MODER &= ~(0x03<<18);  //PF9, 18,19位清零
 	GPIOF->MODER |= 0x01<<18;  //18位置1
-	
-	GPIOF->MODER &= ~(0x03<<20);  //PF10, 20,21位清零
-	GPIOF->MODER |= 0x01<<20;  //20位置1
-	
 	GPIOF->OTYPER &= ~(0x01<<9);  //推挽输出
-	GPIOF->OTYPER &= ~(0x01<<10);
-	
 	GPIOF->OSPEEDR &= ~(0x03<<18);  //输出最大速度2M
-	
 	GPIOF->PUPDR &= ~(0x03<<18); //无上下拉
 	
+	//设置管脚PF10----D6设置为普通功能、推挽、无上下拉功输出
+	GPIOF->MODER &= ~(0x03<<20);  //PF10, 20,21位清零
+	GPIOF->MODER |= 0x01<<20;  //20位置1
+	GPIOF->OTYPER &= ~(0x01<<10);
+	GPIOF->OSPEEDR &= ~(0x03<<20);  //输出最大速度2M
+	GPIOF->PUPDR &= ~(0x03<<20); //无上下拉
+	
 	//设定初始状态---非工作状态
-	GPIOF->ODR |= 0x03<<9;
+	GPIOF->ODR |= 0x03<<9;  //控制PF9,PF10高电平,D5,D6灭
 }
 
 //延时函数
@@ -37,11 +55,11 @@ int main(void)
 	
 	while(1)
 	{
-		GPIOF->ODR &= ~(0x01<<9);  //LED ON
-		GPIOF->ODR &= ~(0x01<<10);  //LED ON
+		D5 = 0;  //低电平,LED ON
+		D6 = 0;  //低点平LED ON
 		delay();
-		GPIOF->ODR |= 0x01<<9;  //LED OFF
-		GPIOF->ODR |= 0x01<<10;  //LED OFF
+		D5 = 1;  //LED OFF
+		D6 = 1;  //LED OFF
 		delay();
 		
 	}
