1539665f778441b8d682930ba65436cb40ad7363
bit_harrywu <wuxiaohuaer@gmail.com>
2019-04-21 09:45:49 +0800

    ex2, ex3用新版uVersion重新打开过，删除ex3 - usrt - string-V5.20

--- ex3 - usrt - string - V5.20/user/inc/delay.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DELAY_H_
-#define _DELAY_H_
-#include "stm32f4xx.h"
-
-void delay_ms(int t);
-	
-#endif
--- ex3 - usrt - string - V5.20/user/inc/led.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LED_H_
-#define _LED_H_
-
-#define D5  PFout(9)
-#define D6  PFout(10)
-
-void led_init(void);
-
-#endif
